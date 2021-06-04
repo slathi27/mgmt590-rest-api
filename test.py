076de36531c6125bc63a9a12318fb2c35048bde6
slathi27 <84410969+slathi27@users.noreply.github.com>
2021-06-03 21:17:59 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,74 +1,114 @@
-#Importing Libraries
-import pytest
+#Import libraries
 import json
-from answer import create_app
+import pytest
+import time
+import sqlite3
+from transformers.pipelines import pipeline
+from flask import Flask
+from flask import request, jsonify
 import os
 import psycopg2
+from app import create_app
+
+# create pem files
+file = open("/server-c.pem", "w")
+root_cert_variable = os.getenv['PG_SSLROOTCERT']
+root_cert_variable = root_cert_variable.replace('@', '=')
+file.write(root_cert_variable)
+file.close()
+
+file = open("/client-cert.pem", "w")
+cert_variable = os.environ['PG_SSLCERT']
+cert_variable = cert_variable.replace('@', '=')
+file.write(cert_variable)
+file.close()
+
+file = open("/client-key.pem", "w")
+client_key = os.environ['PG_SSLKEY']
+client_key = client_key.replace('@', '=')
+file.write(client_key)
+file.close()
+
+os.chmod("/client-key.pem", 0o600)
+os.chmod("/client-cert.pem", 0o600)
+os.chmod("/server-c.pem", 0o600)
+
+# Format DB connection information
+sslmode = "sslmode=verify-ca"
+sslrootcert = "sslrootcert=/server-c.pem"
+sslcert = "sslcert=/client-cert.pem"
+sslkey = "sslkey=/client-key.pem"
+hostaddr = "hostaddr={}".format(os.environ['PG_HOST'])
+user = "user=postgres"
+password = "password={}".format(os.environ['PG_PASSWORD'])
+dbname = "dbname=psdpdb"
+
+# sslmode = "sslmode=verify-ca"
+# sslrootcert = "sslrootcert=server-ca.pem"
+# sslcert = "sslcert=client-cert.pem"
+# sslkey = "sslkey=client-key.pem"
+# hostaddr = "hostaddr=35.188.117.213"
+# user = "user=postgres"
+# password = "password=psdp1234"
+# dbname = "dbname=psdpdb"
+
+# Construct database connect string
+db_connect_string = " ".join([
+    sslmode,
+    sslrootcert,
+    sslcert,
+    sslkey,
+    hostaddr,
+    user,
+    password,
+    dbname
+])
 
 @pytest.fixture
-# Function To test on answer.py function - create_app
-def test_client():
+def client():
     app = create_app()
     app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
 
-# Test For Route - Models /GET
-def test_get_models(client):
-    # 1
-    # Testing For Get Functionality. 200 Status Code - API Connection Worked Successfully
+
+#Function to test the get method of the route '/models'
+def test_modelsget(client):
     rv = client.get("/models")
-    assert rv.status_code == 200
-    # 2
-    # Output of Get Function Matches The Default Model In The Database
-    expected = [{'model': 'distilbert-base-uncased-distilled-squad', 'name': 'distilled-bert',
-                 'tokenizer': 'distilbert-base-uncased-distilled-squad'}]
-    assert expected == rv.json
+    assert 200 == rv.status_code
 
-# Test For Route - Models /PUT
-def test_put_models(client):
-    # 1
-    # Testing For Put Functionality. 200 Status Code - API Connection Worked Successfully
-    url = "/models"
-    info = {
-        "name": "bert-tiny",
-        "tokenizer": "mrm8488/bert-tiny-5-finetuned-squadv2",
-        "model": "mrm8488/bert-tiny-5-finetuned-squadv2"
-    }
-    rv = client.put(url, json=info)
-    assert rv.status_code == 200
 
-# Test For Route - Models /DELETE
-def test_delete_models(client):
-    #1
-    # Model Name Not Inserted Or Trying To Delete Default Model
-    rv = client.delete("/models")
-    assert rv.status_code == 400
-    #2
-    # Testing For Delete Functionality. 200 Status Code - API Connection Worked Successfully
-    # Deleting Model From Database By Entering A Model Name
-    rv2 = client.delete("/models?model=bert-tiny")
-    assert rv2.status_code == 200
+#Function to test the put method by inserting a model into the database
+def test_modelsput(client):
+    data = {"name":"bert-tiny",
+            "tokenizer":"mrm8488/bert-tiny-5-finetuned-squadv2",
+            "model":"mrm8488/bert-tiny-5-finetuned-squadv2"}
+    rv = client.put("/models", json = data)
+    assert 200 == rv.status_code
 
-# Test For Route - Answer /POST
-def test_post_answer(client):
-    url = "/answer?model=distilled-bert"
-    info = {
-        "question" : "Who did holly matthews play in waterloo rd?",
-        "context": "She attended the British drama school East 15 in 2005, and left after winning a high-profile role in the BBC drama Waterloo Road, playing the bully Leigh-Ann Galloway"
-    }
-    rv = client.post(url, json=info)
-    #Checking if correct model is being used
-    modelname = json.loads(rv.data)['model']
-    assert modelname == 'distilled-bert'
 
-# Test For Route - Answer /GET
-def test_get_answer(client):
-    # 1
-    # Testing For Getting Answers Functionality. 200 Status Code - API Connection Worked Successfully
-    rv2 = client.get("/answer?start=1&end=9999999999")
-    assert rv2.status_code == 200
-    # #2
-    # Fetching Answers From Dataset Without Providing Timestamps - Query 400
-    rv = client.get("/answer")
-    assert rv.status_code == 400
+#Trying to delete the default model which throws a 400 error
+def test_modelsdel(client):
+    rv = client.delete("/models?model=distilled-bert")
+    assert 400 == rv.status_code
+
+
+#Function to test the Question Answering api if it is returning the correct ouput
+def test_answerpost(client):
+    data = {"question": "who did holly matthews play in waterloo rd?",
+            "context": "She attended the British drama school East 15 in 2005, and left after winning a high-profile role in the BBC drama Waterloo Road, playing the bully Leigh-Ann Galloway.[6] Since that role, Matthews has continued to act in BBC's Doctors, playing Connie Whitfield; in ITV's The Bill playing drug addict Josie Clarke; and she was back in the BBC soap Doctors in 2009, playing Tansy Flack"}
+    rv = client.post('/answer?model=distilled-bert', json=data)
+    assert json.loads(rv.data)['answer'] == "Leigh-Ann Galloway"
+    assert 200 == rv.status_code
+
+
+#Function to test the get method of Question Answering by not passing the start and end parameters in the url
+#It throws a 400 error
+def test_answerget(client):
+    rv = client.get("/answer?model=distilled-bert")
+    assert 400 == rv.status_code
+
+#Function to test if GET is fetching the result
+def test_answergetresult(client):
+    rv = client.get("/answer?start=0&end=9999999999")
+    assert 200 == rv.status_code
